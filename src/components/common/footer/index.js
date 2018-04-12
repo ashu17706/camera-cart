a3a0b47cb908d7bc4d54dce71eb9a9eea21b74a0
Ashutosh Tripathi <ashu17706@gmail.com>
2018-04-13 05:05:19 +0530

    finilizing footer

--- src/components/common/footer/index.js
+++ src/components/common/footer/index.js
@@ -4,5 +4,35 @@ import classNames from 'classnames';
 import './footer.css';
 
 export default () => (
-  <div className={classNames('footer')}>Footer</div>
+  <div className={classNames("footer")}>
+    <p>Made with ♥️ in Hyderabad [IN]</p>
+    <p>
+      Built with&nbsp;
+      <a target="_blank" href="https://github.com/reactjs/redux">
+        react
+      </a>
+      &nbsp;
+      <a target="_blank" href="https://github.com/reactjs/react-redux">
+        redux
+      </a>
+      &nbsp;
+      <a target="_blank" href="https://github.com/reactjs/react-redux">
+        react-redux
+      </a>
+      &nbsp;
+      <a target="_blank" href="https://github.com/ReactTraining/react-router">
+        react-router
+      </a>
+      &nbsp;
+      <a target="_blank" href="https://github.com/redux-saga/redux-saga">
+        redux-saga
+      </a>
+      &nbsp;
+      and
+      &nbsp;
+      <a target="_blank" href="https://github.com/moltin/js-sdk">
+        moltin
+      </a>
+    </p>
+  </div>
 );
