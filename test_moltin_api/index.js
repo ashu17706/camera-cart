b419159a5f218910123b1f66e3f399118411867f
Ashutosh Tripathi <ashu17706@gmail.com>
2018-04-12 16:35:26 +0530

    testing moltin api on node [WIP]

---
+++ test_moltin_api/index.js
@@ -0,0 +1,12 @@
+const MoltinGateway = require('@moltin/sdk').gateway;
+
+const Moltin = MoltinGateway({
+  client_id: process.env.MOLTIN_CLIENT_ID,
+  client_secret: process.env.MOLTIN_CLIENT_SECRET
+});
+
+Moltin
+  .Authenticate()
+  .then(response => {
+    console.log(`Authenticated, ${JSON.stringify(response, null, 2)}`);
+  });
